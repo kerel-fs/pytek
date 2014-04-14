518d484a24adf804a2cc665ac3b40c221e891bd0
Brian Mearns <mearns@users.noreply.github.com>
2014-04-13 21:38:06 -0400

    Fixed distribution tests to work for non-releases as well.

--- test/test_dist.py
+++ test/test_dist.py
@@ -15,6 +15,7 @@ import pytek
 import pytek.version
 
 
+changes_prerelease_re = re.compile(r'^Pre[ -]?Rel\s+(?P<release>\d+)\s*$', re.I)
 changes_heading_re = re.compile(r'^Rel\s+(?P<release>\d+)\s+- v(?P<M>\d+).(?P<n>\d+).(?P<p>\d+).(?P<s>\d+) - (?P<year>\d\d\d\d)-(?P<month>\d\d)-(?P<day>\d\d)\s*$')
 
 
@@ -33,22 +34,31 @@ def test_changes_dot_txt_top_version():
     with open("CHANGES.txt", "r") as changes:
         for line in changes:
             if len(line.strip()):
-                mobj = changes_heading_re.match(line)
-                assert(mobj is not None), line
-
+                mobj = changes_prerelease_re.match(line)
                 def match_number(group_name, against):
                     assert(int(mobj.group(group_name)) == against)
 
-                match_number('release', pytek.version.RELEASE)
-                match_number('M', pytek.version.MAJOR)
-                match_number('n', pytek.version.MINOR)
-                match_number('p', pytek.version.PATCH)
-                match_number('s', pytek.version.SEMANTIC)
-                match_number('year', pytek.version.YEAR)
-                match_number('month', pytek.version.MONTH)
-                match_number('day', pytek.version.DAY)
+                if mobj is not None:
+                    #Pre-releases (development versions)
+                    match_number('release', pytek.version.RELEASE + 1)
+                    assert(pytek.version.TAG is not None)
+                    
+                else:
+                    #If it doesn't match the pre-release, should match the release.
+                    mobj = changes_heading_re.match(line)
+                    assert(mobj is not None), line
+
+                    match_number('release', pytek.version.RELEASE)
+                    match_number('M', pytek.version.MAJOR)
+                    match_number('n', pytek.version.MINOR)
+                    match_number('p', pytek.version.PATCH)
+                    match_number('s', pytek.version.SEMANTIC)
+                    match_number('year', pytek.version.YEAR)
+                    match_number('month', pytek.version.MONTH)
+                    match_number('day', pytek.version.DAY)
+                    assert(pytek.version.TAG is None)
 
                 break
 
-            
+                
 
